01a37ca2ec9433eee18449181c0a300c5383236d
Mbaamir <Mbaamir21@gmail.com>
2022-06-13 23:10:32 +0500

    Initial commit

---
+++ configuration_help/Material UI/README.md
@@ -0,0 +1,10 @@
+# Webpack
+
+Have to configure Webpack according to [this](https://mui.com/material-ui/guides/styled-engine/): 
+-Do as follows:
+
+Copy/Replace the attached `webpack.config.js` into `\projectName\node_modules\react-scripts\config`
+
+`tsconfig.json` is configured
+
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Create React App With</h1>
+      <ul>
+        <li>React@18.1.0</li>
+        <li>Typescript</li>
+        <li>Material UI With Styled Components Styling Engine</li>
+        <li>Styled Components </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
